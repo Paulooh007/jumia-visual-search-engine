2b03d0f8024fddbc136babc25524972f83aecdd1
paul <okewunmipaul@yahoo.com>
2023-02-23 23:11:47 +0100

    :tada: Initialize project
    Setup project structure for proper handeling of code and data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A visual search engine for Jumia that lets users search for products by uploading an image. It uses computer vision to find similar or identical products within the store's inventory, saving users time and providing a more personalized shopping experience.',
+    author='Paul Okewunmi',
+    license='',
+)
