01960a5d50a509b6492fc7bdfaaac6b2853d1b66
paul <okewunmipaul@yahoo.com>
2023-06-10 21:14:14 +0100

    Format repo using black

--- image_search_engine/data/crawler_jumia.py
+++ image_search_engine/data/crawler_jumia.py
@@ -1,38 +1,38 @@
 import scrapy
 from pathlib import Path
-from selenium.webdriver import Chrome, ChromeOptions 
+from selenium.webdriver import Chrome, ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
 
 from scrapy.crawler import CrawlerProcess
 
 from metadata import PRODUCT_BASE_URLS, PRODUCT_CATEGORIES, MAX_PAGE
 
+
 class ProductsSpider(scrapy.Spider):
-    name = 'products'
+    name = "products"
     product_names = PRODUCT_CATEGORIES
     urls_list = PRODUCT_BASE_URLS
 
     raw_data_dir = Path(__file__).resolve().parents[2] / "data" / "raw"
 
     custom_settings = {
-    "FEEDS": {
-        f"{raw_data_dir}/raw.csv": {"format": "csv"},
-        f"{raw_data_dir}/raw.json": {"format": "json"},
-    },
-    "FEED_EXPORT_ENCODING": 'utf-8'
+        "FEEDS": {
+            f"{raw_data_dir}/raw.csv": {"format": "csv"},
+            f"{raw_data_dir}/raw.json": {"format": "json"},
+        },
+        "FEED_EXPORT_ENCODING": "utf-8",
     }
 
     def start_requests(self):
-
         options = ChromeOptions()
         options.headless = True
-        driver = Chrome(ChromeDriverManager().install(), options = options)
+        driver = Chrome(ChromeDriverManager().install(), options=options)
 
         MAX_PAGES = MAX_PAGE
 
         def get_url_for_page(BASE_URL, page_num):
             return f"{BASE_URL}&page={page_num}#catalog-listing"
-        
+
         count = 0
         for product_name, base_url in zip(self.product_names, self.urls_list):
             for page in range(1, MAX_PAGES + 1):
@@ -44,7 +44,15 @@ class ProductsSpider(scrapy.Spider):
 
                     for link in product_links:
                         href = link.get_attribute("href")
-                        yield scrapy.Request(href, callback=self.parse, meta = {"page_no": page, "product_category": product_name.lower(), "count": str(count)})
+                        yield scrapy.Request(
+                            href,
+                            callback=self.parse,
+                            meta={
+                                "page_no": page,
+                                "product_category": product_name.lower(),
+                                "count": str(count),
+                            },
+                        )
                         count += 1
 
                 except:
@@ -52,17 +60,18 @@ class ProductsSpider(scrapy.Spider):
 
         driver.quit()
 
-
     def parse(self, response):
         product_category = response.meta["product_category"]
         name = response.xpath("//h1[@class='-fs20 -pts -pbxs']/text()").get()
-        discounted_price = response.xpath("//span[@class='-b -ltr -tal -fs24']/text()").get()
-        original_price = response.xpath("//span[@class='-tal -gy5 -lthr -fs16']/text()").get()
+        discounted_price = response.xpath(
+            "//span[@class='-b -ltr -tal -fs24']/text()"
+        ).get()
+        original_price = response.xpath(
+            "//span[@class='-tal -gy5 -lthr -fs16']/text()"
+        ).get()
         product_images = response.xpath("//div[@id='imgs']/a/@href").getall()
         page_no = response.meta["page_no"]
         count = response.meta["count"]
-
-
 
         yield {
             "product_id": f"product_{product_category}_{count}",
@@ -72,12 +81,11 @@ class ProductsSpider(scrapy.Spider):
             "discounted_price": discounted_price,
             "original_price": original_price,
             "product_images_url": product_images,
-            "page_no": page_no
-
+            "page_no": page_no,
         }
+
 
 if __name__ == "__main__":
     process = CrawlerProcess()
     process.crawl(ProductsSpider)
     process.start()
-
